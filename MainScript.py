3f7e146aaf9ef0d7dc59b521bf60f56182582c61
machen <machen27@gmail.com>
2018-12-12 14:52:47 -0500

    Added Data Read in
    All data for a region of interest is added to a master table, which indexes the element and timepoint taken

--- MainScript.py
+++ MainScript.py
@@ -5,15 +5,21 @@ import re
 
 # Step 1: Select the files from the region that we want to examine
 
-dataFileLocation = """C:\\Users\\Michael\\OneDrive\\Archive
-                      \\MIT Grad School Research Overflow\\Microfluidics
-                      \\NSLS-II Data\\Kocar_NSLS-II_October15_2018_EndOfRun
-                      \\01010_Kocar\\Maps\\Exported Data
-                      \\Device 6G Normalized by I0"""
+dataFileLocation = """C:\\Users\\Michael\\OneDrive\\Archive\\MIT Grad School Research Overflow\\Microfluidics\\NSLS-II Data\\Kocar_NSLS-II_October15_2018_EndOfRun\\01010_Kocar\\Maps\\Exported Data\\Device 6G Normalized by I0\\"""
 region = "Region1"  # CHANGE THIS
 timePoints = ["AGWRinse", "AsFilled", "SFlush"]
 availableFiles = os.listdir(dataFileLocation)
+data = pd.DataFrame(columns=["Time Point", "Element", "X", "Y", "Intensity"])
 for timePoint in timePoints:
     # Selects for files that match the region and timepoint of interest
     filePat = re.compile(".*_("+timePoint+")_("+region+")_.*_(\D{2})_Ka.dat")
     for name in availableFiles:
+        match = re.match(filePat, name)
+        if match:
+            newData = pd.read_table(dataFileLocation+name, skiprows=[0,1,2],
+                                    header=None, names=['Y', 'X', 'Intensity'],
+                                    sep="\s+")
+            newData.loc[:, "Time Point"] = timePoint
+            newData.loc[:, "Element"] = match.group(3)
+            data = data.append(newData, ignore_index=True)
+
