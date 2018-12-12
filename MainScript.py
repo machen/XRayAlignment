b50331566287dabe2cdc504a5c95085628c43b7e
machen <machen27@gmail.com>
2018-12-12 17:38:44 -0500

    Added alignment params, add iteration to find matching points

--- MainScript.py
+++ MainScript.py
@@ -10,6 +10,7 @@ region = "Region1"  # CHANGE THIS
 timePoints = ["AGWRinse", "AsFilled", "SFlush"]
 availableFiles = os.listdir(dataFileLocation)
 data = pd.DataFrame(columns=["Time Point", "Element", "X", "Y", "Intensity"])
+
 for timePoint in timePoints:
     # Selects for files that match the region and timepoint of interest
     filePat = re.compile(".*_("+timePoint+")_("+region+")_.*_(\D{2})_Ka.dat")
@@ -23,3 +24,32 @@ for timePoint in timePoints:
             newData.loc[:, "Element"] = match.group(3)
             data = data.append(newData, ignore_index=True)
 
+# Apply calculated displacements to data
+fillingData = data.loc[:, data.loc[:, "Time Point"=="AsFilled"]]
+# fillingData = fillingData.loc[:, ["X", "Y", "Intensity"]].values
+rinseData = data.loc[:, data.loc[:, "Time Point"=="AGWRinse"]]
+rinseData.loc[:, "X"] = rinseData.loc[:, "X"]+0.025
+rinseData.loc[:, "Y"] = rinseData.loc[:, "Y"]-0.005
+# rinseData = rinseData.loc[:, ["X", "Y", "Intensity"]].values
+sulfideData = data.loc[:, data.loc[:, "Time Point"=="SFlush"]]
+sulfideData.loc[:, "X"] = sulfideData.loc[:, "X"]+0.040
+sulfideData.loc[:, "Y"] = sulfideData.loc[:, "Y"]-0.01
+# sulfideData = sulfideData.loc[:, ["X", "Y", "Intensity"]].values
+
+
+#Calculate differences
+difference = pd.DataFrame(columns=["X","Y","FillingRinse","RinseSulfide"])
+for row in fillingData.iterrows():
+    X = row["X"]
+    Y = row["Y"]
+    element = row["Element"]
+    fillIntensity = row["Intensity"]
+    # Calculate Filling Minus Rinse
+    subData = rinseData.loc[:, rinseData.loc[:,"Element"==element]]
+    subData = subData.loc[abs(subData.loc[:,'X']-X)<0.004,:]
+    subData = subData.loc[abs(subData.loc[:,'Y']-Y)<0.004,:]
+    res = len(subData)
+    if res == 1:
+        rinseIntensity = subData.loc[:, "Intensity"]
+    elif res == 0:
+        continue
