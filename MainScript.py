cb6152eadc6e23adc847bbea294bc3a0ea1bbcf6
machen <machen27@gmail.com>
2018-12-13 15:26:00 -0500

    MainScript produces the desired result matrix
    Matrix contains the matched X/Y coordinates, as well as the calculated differences in the desired intensities

--- MainScript.py
+++ MainScript.py
@@ -7,6 +7,8 @@ import re
 
 dataFileLocation = """C:\\Users\\Michael\\OneDrive\\Archive\\MIT Grad School Research Overflow\\Microfluidics\\NSLS-II Data\\Kocar_NSLS-II_October15_2018_EndOfRun\\01010_Kocar\\Maps\\Exported Data\\Device 6G Normalized by I0\\"""
 region = "Region1"  # CHANGE THIS
+rinseAdjust = [0.025, -0.005]  # Adjustment to align AGWRinse with AsFilled [x, y] in mm
+sulfideAdjust = [0.040, -0.01]  # Adjustment to align SulfideFlush with AsFilled
 timePoints = ["AGWRinse", "AsFilled", "SFlush"]
 availableFiles = os.listdir(dataFileLocation)
 data = pd.DataFrame(columns=["Time Point", "Element", "X", "Y", "Intensity"])
@@ -17,39 +19,63 @@ for timePoint in timePoints:
     for name in availableFiles:
         match = re.match(filePat, name)
         if match:
-            newData = pd.read_table(dataFileLocation+name, skiprows=[0,1,2],
+            newData = pd.read_table(dataFileLocation+name, skiprows=[0, 1, 2],
                                     header=None, names=['Y', 'X', 'Intensity'],
                                     sep="\s+")
             newData.loc[:, "Time Point"] = timePoint
             newData.loc[:, "Element"] = match.group(3)
             data = data.append(newData, ignore_index=True)
-
+data = data.round({'X': 3, 'Y': 3})
 # Apply calculated displacements to data
-fillingData = data.loc[:, data.loc[:, "Time Point"=="AsFilled"]]
+fillingData = data.loc[data.loc[:, "Time Point"] == "AsFilled", :]
 # fillingData = fillingData.loc[:, ["X", "Y", "Intensity"]].values
-rinseData = data.loc[:, data.loc[:, "Time Point"=="AGWRinse"]]
-rinseData.loc[:, "X"] = rinseData.loc[:, "X"]+0.025
-rinseData.loc[:, "Y"] = rinseData.loc[:, "Y"]-0.005
+rinseData = data.loc[data.loc[:, "Time Point"] == "AGWRinse", :]
+rinseData.loc[:, "X"] = rinseData.loc[:, "X"]+rinseAdjust[0]
+rinseData.loc[:, "Y"] = rinseData.loc[:, "Y"]+rinseAdjust[1]
 # rinseData = rinseData.loc[:, ["X", "Y", "Intensity"]].values
-sulfideData = data.loc[:, data.loc[:, "Time Point"=="SFlush"]]
-sulfideData.loc[:, "X"] = sulfideData.loc[:, "X"]+0.040
-sulfideData.loc[:, "Y"] = sulfideData.loc[:, "Y"]-0.01
+sulfideData = data.loc[data.loc[:, "Time Point"] == "SFlush", :]
+sulfideData.loc[:, "X"] = sulfideData.loc[:, "X"]+sulfideAdjust[0]
+sulfideData.loc[:, "Y"] = sulfideData.loc[:, "Y"]+sulfideAdjust[1]
 # sulfideData = sulfideData.loc[:, ["X", "Y", "Intensity"]].values
 
 
-#Calculate differences
-difference = pd.DataFrame(columns=["X","Y","FillingRinse","RinseSulfide"])
-for row in fillingData.iterrows():
-    X = row["X"]
-    Y = row["Y"]
-    element = row["Element"]
-    fillIntensity = row["Intensity"]
-    # Calculate Filling Minus Rinse
-    subData = rinseData.loc[:, rinseData.loc[:,"Element"==element]]
-    subData = subData.loc[abs(subData.loc[:,'X']-X)<0.004,:]
-    subData = subData.loc[abs(subData.loc[:,'Y']-Y)<0.004,:]
+# Calculate differences
+
+differenceMat = []
+for i in fillingData.index:
+    X = fillingData.loc[i, "X"]
+    Y = fillingData.loc[i, "Y"]
+    element = fillingData.loc[i, "Element"]
+    fillIntensity = fillingData.loc[i, "Intensity"]
+    # Calculate Filling Minus Rinse, first select for the coordinate
+    subData = rinseData.loc[rinseData.loc[:, "Element"] == element, :]
+    subData = subData.loc[(abs(subData.loc[:,
+                          'X']-X) < 0.0001) & (abs(subData.loc[:,
+                          'Y']-Y) < 0.0001), :]
+    point = subData.index
     res = len(subData)
+    # Test to make sure we have the correct amount
     if res == 1:
-        rinseIntensity = subData.loc[:, "Intensity"]
+        rinseIntensity = float(subData.loc[point, "Intensity"])
     elif res == 0:
         continue
+    else:
+        raise ValueError('Non-unique results during coordinate search')
+
+    subData = sulfideData.loc[sulfideData.loc[:, "Element"] == element, :]
+    subData = subData.loc[(abs(subData.loc[:,
+                               'X']-X) < 0.0001) & (abs(subData.loc[:,
+                               'Y']-Y) < 0.0001), :]
+    point = subData.index
+    res = len(subData)
+    if res == 1:
+        sulfideIntensity = float(subData.loc[point, "Intensity"])
+    elif res == 0:
+        continue
+    else:
+        raise ValueError('Non-unique results during coordinate search')
+    differenceMat.append([X, Y, rinseIntensity - fillIntensity,
+                          sulfideIntensity - rinseIntensity, element])
+    print(i)
+difference = pd.DataFrame(differenceMat, columns=["X", "Y", "FillingRinse",
+                          "RinseSulfide", "element"])
